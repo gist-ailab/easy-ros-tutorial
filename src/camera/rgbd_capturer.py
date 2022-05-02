fea986b570e30144dca3b1defc416ccb8122f95a
seungback <shback@gm.gist.ac.kr>
2022-05-02 17:13:41 +0900

    feat: Save o3d pc

--- src/camera/rgbd_capturer.py
+++ src/camera/rgbd_capturer.py
@@ -4,8 +4,10 @@ import rospy
 import cv2, cv_bridge
 import numpy as np
 import message_filters
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, PointCloud2
 import os
+import open3d as o3d
+from open3d_ros_helper import open3d_ros_helper as orh
 
     
 if __name__ == '__main__':
@@ -16,8 +18,8 @@ if __name__ == '__main__':
     bridge = cv_bridge.CvBridge()
     
     img_id = 0
-    save_root = "./"
-    min_depth, max_depth = 0.25, 3.5
+    save_root = "./data"
+    min_depth, max_depth = 250, 3500
     print("min_depth: " + str(min_depth) + "  max_depth: " + str(max_depth))
 
     while True:
@@ -25,6 +27,7 @@ if __name__ == '__main__':
         # get rgb-depth from camera
         rgb_msg = rospy.wait_for_message("/rgb/image_raw", Image)
         depth_msg = rospy.wait_for_message("/depth_to_rgb/image_raw", Image)
+        pc_msg = rospy.wait_for_message("/points2", PointCloud2)
         rgb = bridge.imgmsg_to_cv2(rgb_msg,desired_encoding='bgr8')
         depth = bridge.imgmsg_to_cv2(depth_msg, desired_encoding='32FC1')
 
@@ -47,6 +50,9 @@ if __name__ == '__main__':
             cv2.imwrite(os.path.join(save_root, "rgb_{}.png".format(img_id)), rgb)
             cv2.imwrite(os.path.join(save_root, "depth_im_{}.png".format(img_id)), depth_im)
             np.save(os.path.join(save_root, "depth_{}.npy".format(img_id)), depth)
+            o3dpc = orh.rospc_to_o3dpc(pc_msg) 
+            o3d.io.write_point_cloud(os.path.join(save_root, "o3d_cloud_{}.ply".format(img_id)), o3dpc)
+            o3d.visualization.draw_geometries([o3dpc])
             img_id += 1
             cv2.destroyAllWindows()
             
